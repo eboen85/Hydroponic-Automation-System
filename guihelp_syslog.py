4fd96370e54bca64be41bd543759a1ce0afca1c3
eboen85 <eeb04170@vtc.edu>
2016-03-08 16:55:03 -0500

    Added files via upload

---
+++ guihelp_syslog.py
@@ -0,0 +1,12 @@
+import sys
+
+def main(self):
+
+  #This script is called by system scripts and outputs to gui_has.py
+  #This script may also be called by other GUI helper scripts to output something to syslog
+
+  #feed string from parent script to this script - anyone can write
+  #code here
+  
+  #output string to syslog textbox in GUI - Dan will write
+  #code here
