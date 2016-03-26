c29adddd5ce7916948b2666ca8f2010f541a8a4a
eboen85 <eeb04170@vtc.edu>
2016-03-26 11:59:16 -0400

    Update gui_has_rework.py

--- gui_has_rework.py
+++ gui_has_rework.py
@@ -10,7 +10,15 @@ class HAS(QtGui.QWidget):
         
         
     def initUI(self):
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+		
+		self.setToolTip('This is a <b>QWidget</b> widget')
         
+        btn = QtGui.QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+		
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('HAS')
         self.setWindowIcon(QtGui.QIcon('leaflogotest.pgn'))        
