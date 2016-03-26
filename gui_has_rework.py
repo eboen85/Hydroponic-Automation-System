f40aaf671515e5841381e7c1aea46a8dce6fc315
eboen85 <eeb04170@vtc.edu>
2016-03-26 12:29:36 -0400

    Update gui_has_rework.py

--- gui_has_rework.py
+++ gui_has_rework.py
@@ -10,24 +10,61 @@ class HAS(QtGui.QWidget):
         
         
     def initUI(self):
+	
+	    exitAction = QtGui.QAction(QtGui.QIcon('exit24.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
 		
 		self.setToolTip('This is a <b>QWidget</b> widget')
         
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn = QtGui.QPushButton('Pump', self)
+        btn.setToolTip('Starts pump script')
         btn.resize(btn.sizeHint())
-        btn.move(50, 50)
+        btn.move(25, 25)
+		
+		btn = QtGui.QPushButton('pH Meter', self)
+        btn.setToolTip('Starts pH script')
+        btn.resize(btn.sizeHint())
+        btn.move(25, 50)
+		
+		btn = QtGui.QPushButton('Light', self)
+        btn.setToolTip('Starts light script')
+        btn.resize(btn.sizeHint())
+        btn.move(25, 75)
 		
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('HAS')
         self.setWindowIcon(QtGui.QIcon('leaflogotest.pgn'))        
     
         self.show()
-	def main():
-		app = QtGui.QApplication(sys.argv)
-		gui = HAS()                          #just a handy label for the window
-		sys.exit(app.exec_())
+	
+	def closeEvent(self, event):
+        
+        reply = QtGui.QMessageBox.question(self, 'Message',
+            "Are you sure you want to close the HAS", QtGui.QMessageBox.Yes | 
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+		
+		
+def main():
+	app = QtGui.QApplication(sys.argv)
+	gui = HAS()                          #just a handy label for the window
+	sys.exit(app.exec_())
   
   
 if __name__ == '__main__':
